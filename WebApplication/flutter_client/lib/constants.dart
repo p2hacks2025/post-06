c1fcb89b3541f4296135cb8b62bfd8065601102b
Wadacchan <b1023006@fun.ac.jp>
2025-12-19 01:07:17 +0900

    titleページのUI（画像以外）

--- WebApplication/flutter_client/lib/constants.dart
+++ WebApplication/flutter_client/lib/constants.dart
@@ -1,2 +1,2 @@
-const baseUrl = "https://buck-realtor-luggage-longest.trycloudflare.com";
+const baseUrl = "https://arrange-solution-dis-saved.trycloudflare.com";
 const String unlockKey = "OnionSalmon2025";
